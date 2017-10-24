1955c172505da4d030237ecf891f22ea10efb83b
LiveNL <jordiwippert@live.nl>
2017-10-24 15:43:13 +0200

    Add (block) coordinates, inBlock (player) and empty move

--- src/Main.hs
+++ src/Main.hs
@@ -75,6 +75,20 @@ handleKeys _                                         game = game
 update :: Float -> GTA -> GTA
 update _ game = game
 
+canMove :: (Float, Float) -> Bool
+canMove (x,y) = not (inBlock (x,y) (coordinates block))
+
+inBlock :: (Float, Float) -> Path -> Bool
+inBlock (x,y) [(x1,y1), _, (x2,y2), _]
+  | x >= x1 && x <= x2 && y >= y1 && y <= y2 = True
+  | otherwise = False
+
+coordinates :: Picture -> Path
+coordinates (Translate x y color) = color' x y color
+  where color' x y (Color _ polygon) = polygon' x y polygon
+        polygon' x y (Polygon xs) = map (f x y) xs
+        f x y z = (fst z + x, snd z + y)
+
 block :: Picture
 block = translate (-15) 0 $ color white $ rectangleSolid 10 100
 
