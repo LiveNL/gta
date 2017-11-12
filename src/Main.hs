ac6275e149009cda6dd6e94fdf35036ffba89b4d
LiveNL <jordiwippert@live.nl>
2017-11-12 15:38:10 +0100

    Fix merge

--- src/Main.hs
+++ src/Main.hs
@@ -144,7 +144,7 @@ update secs game@Game{player} = do
     GameOver -> return game
     Paused   -> return game
     Init     -> return game
-    Running  -> writeJSON ( return ( timeUp (updateCoins elapsedTime' (updateTraffic rInt (updatePlayerPosition game { elapsedTime = elapsedTime' + secs })))))
+    Running  -> writeHighscore ( return ( timeUp (updateCoins (updateTraffic rInt (updatePlayerPosition game { elapsedTime = elapsedTime' + secs })))))
   where elapsedTime' = elapsedTime game
 
 timeUp :: GTA -> GTA
